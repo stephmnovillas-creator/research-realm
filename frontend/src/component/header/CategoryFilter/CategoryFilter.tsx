7af53acc5aac0ed59e4e3a34fb6e73e14b9fda1d
stephmnovillas-creator <stephmnovillas@gmail.com>
2026-01-19 15:15:33 +0800

    update CategoryFilter

---
+++ frontend/src/component/header/CategoryFilter/CategoryFilter.tsx
@@ -0,0 +1,12 @@
+const CategoryFilter = () => {
+  return (
+    <select style={{ padding: "8px", marginBottom: "10px" }}>
+      <option>All Categories</option>
+      <option>Innovation</option>
+      <option>Life Science</option>
+      <option>Engineering</option>
+    </select> 
+  );
+};
+
+export default CategoryFilter;
