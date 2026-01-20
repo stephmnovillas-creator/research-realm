1460fd6eccbee4ff9822e1ad061fe654d63916b6
stephmnovillas-creator <stephmnovillas@gmail.com>
2026-01-20 23:25:48 +0800

    prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import express from "express";
+import { prisma } from "./lib/prisma";
 const app = express();
 
 app.use(express.json());
@@ -6,7 +8,33 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Research Realm Backend is running");
 });
-
+app.get("/archives",async (req, res) => {
+  const archives= await prisma.research.findMany();
+  res.json(archives);
+});
+app.get("/archives/:id",async (req, res) => {
+  const { id } = req.params;
+  const research = await prisma.research.findUnique({
+    where: { id: Number(id) },
+  });
+  if (research) {
+    res.json(research);
+  } else {
+    res.status(404).json({ error: "Research not found" });
+  }
+});
+app.post("/archives",async (req, res) => {
+  const { title, author, abstract, publishedAt } = req.body;
+  const newResearch = await prisma.research.create({
+    data: {
+      title,
+      author,
+      abstract,
+      publishedAt
+    },
+  });
+  res.status(201).json(newResearch);
+});
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
