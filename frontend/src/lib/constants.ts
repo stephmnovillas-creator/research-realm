da59ecd14578ad145b54af39ccf0ab7a494e9f7d
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-01-25 22:00:55 +0800

    added constants

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,8 @@
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string || "http://localhost:4000";
+
+export const YEARS = {
+  "2018" : '2018',
+  '2023': '2023',
+  '2024': '2024',
+  '2025': '2025'
+} as const;
